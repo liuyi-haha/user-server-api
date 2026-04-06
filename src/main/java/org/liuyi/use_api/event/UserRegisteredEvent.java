80aa7a55a78f4ff691a653a3eec66ea0c8aff11d
liuyi <3319233753@qq.com>
2026-04-07 00:08:51 +0800

    修改事件定义

--- src/main/java/org/liuyi/use_api/event/UserRegisteredEvent.java
+++ src/main/java/org/liuyi/use_api/event/UserRegisteredEvent.java
@@ -1,11 +1,16 @@
 package org.liuyi.use_api.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.liuyi.common.domain.event.Event;
 
-@Builder
 @Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRegisteredEvent extends Event{
     public static final String TOPIC = "user-registered";
     private String userId;
